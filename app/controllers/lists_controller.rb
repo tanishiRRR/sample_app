48256c2fbc57154ceceb43f1acc58823eb59f03c
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-02-09 13:53:49 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,10 +10,11 @@ class ListsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to 'top'
   end
 
   def index
+    @lists = List.all
   end
 
   def show
